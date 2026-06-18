20e861fa1757eea71af5774cea0db2d0ed957cea
Yernazar <erna95er@gmail.com>
2026-06-18 15:40:07 +0500

    add new Response methods

--- response.go
+++ response.go
@@ -11,3 +11,21 @@ type Response struct {
 func Ok(body any) Response      { return Response{Status: 200, Body: body} }
 func Created(body any) Response { return Response{Status: 201, Body: body} }
 func NoContent() Response       { return Response{Status: 204} }
+
+func (r *Response) Ok(body any) Response {
+	r.Status = 200
+	r.Body = body
+	return *r
+}
+
+func (r *Response) Created(body any) Response {
+	r.Status = 201
+	r.Body = body
+	return *r
+}
+
+func (r *Response) NoContent() Response {
+	r.Status = 204
+	r.Body = nil
+	return *r
+}
